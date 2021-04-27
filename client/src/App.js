3419a1b1434360dd4835a240e4f021bc6efd5849
Sean Nguyen <nsean321@gmail.com>
2021-04-27 14:38:43 -0700

    Page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { Home, Login, Register } from './pages'
+import { LockedPage } from './utils'
 
 function App() {
   return (
-    <></>
+    <>
+      {/* NavBar Space */}
+
+
+      {/* Main App */}
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/home' />
+        </Route>
+        <Route exact path='/home'>
+          <LockedPage>
+            <Home />
+          </LockedPage>
+        </Route>
+
+        {/* Login/Registration */}
+        <Route exact path='/login' component={login} />
+        <Route exact path='/register' component={register} />
+
+
+
+
+      </Switch>
+      {/* Footer Space */}
+
+    </>
   );
 }
 
