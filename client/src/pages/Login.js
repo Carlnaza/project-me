7af5d06a2b3bd2843b1a3cc970237e5469932628
Sean Nguyen <nsean321@gmail.com>
2021-04-29 00:22:05 -0700

    Changed Content into Form Component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,87 +1,17 @@
-import { Link } from 'react-router-dom'
-import { Container, Card, Grid, Typography, TextField, Box, Button } from '@material-ui/core'
 
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import { Container } from '@material-ui/core'
 
-import MuiPhoneNumber from 'material-ui-phone-number'
-import { FormContext } from '../utils'
+
 import styles from '../styles/styles.js'
+import Auth from '../components/Auth'
 
 const Login = () => {
   const classes = styles()
 
-  const {
-    login, setLogin,
-    handleLoginInput,
-    handleLoginSubmit,
-    errors
-  } = FormContext()
-
-
   return (
     <main>
       <Container maxWidth='sm'>
-        <Card className={classes.loginCard}>
-          <form onSubmit={handleLoginSubmit}>
-            <Grid container direction="column" >
-              <Typography variant="h4">
-                Login
-              </Typography>
-
-              {/* <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                label="Username"
-                placeholder="john123"
-                type="username"
-                name="username"
-                value={login.username}
-                onChange={handleLoginInput}
-              /> */}
-              <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                type="Email"
-                label="Email Address"
-                placeholder="john123@email.com"
-                name="email"
-                value={login.email}
-                onChange={handleLoginInput}
-              />
-              <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                placeholder="password"
-                type="password"
-                label="Password"
-                name="password"
-                value={login.password}
-                onChange={handleLoginInput}
-              />
-            </Grid>
-            <Button type='submit' onClick={handleLoginSubmit}>Submit</Button>
-            <Link to='/register'>Don't Have an Account? Register Here</Link>
-          </form>
-        </Card>
+        <Auth page='Login' />
       </Container>
     </main>
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,145 +1,19 @@
-import { Link } from 'react-router-dom'
-import { Container, Card, Grid, Typography, TextField, Box, Button } from '@material-ui/core'
 
-import {
-  KeyboardDatePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import { Container } from '@material-ui/core'
 
-import MuiPhoneNumber from 'material-ui-phone-number'
-import { FormContext } from '../utils'
+
 import styles from '../styles/styles.js'
+import Auth from '../components/Auth'
 
 
 const Register = () => {
   const classes = styles()
 
-  const {
-    register, setRegister,
-    dob, setDOB,
-    phone, setPhone,
-    handleRegisterInput,
-    handleRegisterSubmit,
-    errors
-  } = FormContext()
-
 
   return (
     <main>
       <Container maxWidth='sm'>
-        <Card className={classes.loginCard}>
-          <form onSubmit={handleRegisterSubmit}>
-            <Grid container direction="column" >
-              <Typography variant="h4">
-                Register
-              </Typography>
-              <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                placeholder="John Doe"
-                label="Full Name"
-                name="name"
-                value={register.name}
-                onChange={handleRegisterInput}
-              />
-              <Box className={classes.centeredFlex}>
-                <MuiPickersUtilsProvider utils={DateFnsUtils} >
-                  <KeyboardDatePicker
-                    className={classes.halfInput}
-                    required
-                    variant="outlined"
-                    margin="normal"
-                    inputVariant="outlined"
-                    label="Date of Birth"
-                    placeholder="2018/10/10"
-                    value={dob}
-                    onChange={data => setDOB(data.valueOf())}
-                    format="yyyy/MM/dd"
-                  />
-                </MuiPickersUtilsProvider>
-                <MuiPhoneNumber
-                  className={classes.halfInput}
-                  required
-                  disableAreaCodes
-                  variant="outlined"
-                  margin="normal"
-                  defaultCountry={"us"}
-                  label="Phone Number"
-                  name="phone"
-                  value={phone}
-                  onChange={setPhone}
-                />
-              </Box>
-              <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                type="Email"
-                label="Email Address"
-                placeholder="john123@email.com"
-                name="email"
-                value={register.email}
-                onChange={handleRegisterInput}
-              />
-              {/* <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                label="Username"
-                placeholder="john123"
-                name="username"
-                value={register.username}
-                onChange={handleRegisterInput}
-              /> */}
-              <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                placeholder="password"
-                type="password"
-                label="Password"
-                name="password"
-                value={register.password}
-                onChange={handleRegisterInput}
-              />
-              <TextField
-                required
-                fullwidth
-                variant="outlined"
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                placeholder="password"
-                type="password"
-                label="Repeat Password"
-                name="password2"
-                value={register.password2}
-                onChange={handleRegisterInput}
-              />
-            </Grid>
-            <Button type='submit' onClick={handleRegisterSubmit}>Submit</Button>
-            <Link to='/login'>Already Registered? Login Here</Link>
-          </form>
-        </Card>
+        <Auth page='Register' />
       </Container>
     </main>
 
